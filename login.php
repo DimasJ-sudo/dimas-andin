277b4f530bd130486ecbde2fc39c058da054df33
DimasJ-sudo <dimasjpr505@gmail.com>
2026-06-14 09:20:35 +0700

    Delete login.php

--- login.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-require_once __DIR__ . '/../includes/koneksi.php';
-require_once __DIR__ . '/../includes/helpers.php';
-if (!empty($_SESSION['admin_id'])) log_activity($pdo, 'Logout', 'Admin keluar');
-$_SESSION = []; session_destroy();
-header('Location: ../index.php'); exit;
